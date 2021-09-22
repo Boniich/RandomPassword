158346d09e9d3c9c81dfb05ff2ea9c6e687928a6
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2021-09-22 12:05:51 -0300

    create funcionality that generate random number and letter, and vector that mix both

--- src/components/PasswordGenerator.js
+++ src/components/PasswordGenerator.js
@@ -6,20 +6,28 @@ export default class PasswordGenerator extends Component{
     constructor(props){
         super(props);
        this.state = {
-            password: 0,
+            password: [],
         };
 
         this.generar = this.generar.bind(this);
     }
-  
 
     generar(){
+        let array = [];
         let number = 0;
-        //rendondemos numeros menores a 99
+        // rendondemos numeros menores a 99
         number = (Math.round(Math.random()*99));
+        let letters = ["a","b","c","d","e","f","g","h","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+        let letter = letters[(Math.round(Math.random()*(letters.length-1)))]
+        console.log(letter);
+
+        array.push(number);
+        array.push(letter);
+        console.log(array);
 
         this.setState({
-            password: number,
+            password: array,
         })
     }
 
