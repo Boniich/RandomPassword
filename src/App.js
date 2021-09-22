4af4eefcc59cf0075fa55a3a60cb0de63bbf5ed3
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2021-09-22 10:53:23 -0300

    added component <PasswordGenerator> to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Nav from './components/Nav';
+import PasswordGenerator from './components/PasswordGenerator';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Nav />
+        <PasswordGenerator />
       </header>
     </div>
   );
