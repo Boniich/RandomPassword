c453259b14373613f041c3a5b40c3c03667f41d3
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2021-09-22 21:10:30 -0300

    refactored funcions of randomNumbers and randomLetters. Added funcionality that mix numbers and letters and update state

--- src/components/PasswordGenerator.js
+++ src/components/PasswordGenerator.js
@@ -12,19 +12,54 @@ export default class PasswordGenerator extends Component{
         this.generar = this.generar.bind(this);
     }
 
-    generar(){
-        let array = [];
+
+    //  return a random number lower of 99
+    randomNumbers(){
         let number = 0;
-        // rendondemos numeros menores a 99
-        number = (Math.round(Math.random()*99));
+        number = (Math.round(Math.random()*9));
+
+        return number;
+    }
+
+    // return a letter between a ~ z
+    randomLetters(){
+
         let letters = ["a","b","c","d","e","f","g","h","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z"];
 
         let letter = letters[(Math.round(Math.random()*(letters.length-1)))]
         console.log(letter);
 
-        array.push(number);
-        array.push(letter);
+        return letter;
+
+    }
+
+
+    generar(){
+        let array = [];
+        let number = this.randomNumbers();
+        let letter = this.randomLetters();
+
+        for(let i = 0; i<=7; i++){
+
+            // array.push(number);
+            // array.push(letter);
+            array[i] = number;
+            i++;
+            array[i] = letter;
+            number = this.randomNumbers();
+            letter = this.randomLetters();
+            
+            console.log(array.length);
+        }
+            
+       
         console.log(array);
+     
+           
+
+
+       
+
 
         this.setState({
             password: array,
