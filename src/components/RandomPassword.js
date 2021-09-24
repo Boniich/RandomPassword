0943ffba37143cd687f8a19ec31728fd0ed94d10
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2021-09-24 17:46:05 -0300

    added table and some props

--- src/components/RandomPassword.js
+++ src/components/RandomPassword.js
@@ -1,17 +1,22 @@
 import React, { useEffect, useState } from "react"
 import { helpHttp } from "../helpers/helpHttp";
 import DataForm from "./DataForm"
+import DataTable from "./DataTable";
+import Loader from "./Loader";
+import Message from "./Message";
 
 const RandomPassword = () =>{
 
     const [db, setDb] = useState(null);
+    const [dataToEdit, setDataToEdit] = useState(null);
     const [error,setError] = useState(null);
+    const [loading,setLoading] = useState(false);
 
     let api = helpHttp();
     let url = "http://localhost:5000/data"
 
     useEffect(() =>{
-
+        setLoading(true);
         helpHttp().get(url).then((res) =>{
             if(!res.err){
                 setDb(res);
@@ -20,7 +25,8 @@ const RandomPassword = () =>{
                 setDb(null);
                 setError(res);
             }
-        })
+            setLoading(false);
+        });
     }, [url]);
 
 
@@ -39,9 +45,59 @@ const RandomPassword = () =>{
         })
     }
 
+    const updateData = (data) =>{
+        let endPoint = `${url}/${data.id}`;
+
+        let options = {body:data, headers: {"content-type":"application/json"},
+        };
+
+        api.put(endPoint,options).then((res)=>{
+
+            if(!res.err){
+                let newData = db.map((el)=> (el.id === data.id ? data: el));
+                setDb(newData);
+            }else{
+                setError(res);
+            }
+        });
+    }
+
+    const deleteData = (id) =>{
+        let isDelete = window.confirm(`
+        ¿Estas seguro que quieres eliminar la informacion de este sitio?`);
+
+        if(isDelete){
+            let endpoint = `${url}/${id}`;
+            let options = {
+                headers: {"content-type": "application/json"},
+            }
+
+            api.del(endpoint,options).then(res=>{
+
+                if(!res.err){
+                    let newData = db.filter(el => el.id !== id);
+                    setDb(newData);
+                }else{
+                    setError(res);
+                }
+            })
+        }
+    }
+
     return(
         <>
-        <DataForm createData={createData}></DataForm>
+        <DataForm 
+        createData={createData}
+        updateData={updateData}
+        dataToEdit={dataToEdit}
+        setDataToEdit={setDataToEdit}/>
+        {/* por que es necesario el{db &&}
+        */}
+        {loading && <Loader />}
+        {error && (<Message msg={`Error ${error.status}: ${error.statusText}`} bgColor="#dc3545"/>)}
+       {db && <DataTable data={db}
+        setDataToEdit={setDataToEdit}
+        deleteData={deleteData}/>}
         </>
     )
 }
